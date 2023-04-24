4775c52c17fe5ed530c8deb1077ecb4dbb256568
Sanjay <sanjaymanikandan21@gmail.com>
2023-04-24 16:21:03 +0530

    cursor change

--- client/src/Components/objects/servicepage/ServiceCard.js
+++ client/src/Components/objects/servicepage/ServiceCard.js
@@ -211,6 +211,7 @@ const localpath = "http://localhost:3001/";
         
                             )
                         }
+                        
                        
                     }
                     
