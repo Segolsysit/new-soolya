a04e4c9cdf8a212f1a7bec26b4fc5fff72b2c47d
Sanjay M <sanjaymanikandan21@gmail.com>
2023-05-09 10:33:51 +0530

    user completed orders

--- client/src/Components/objects/Userdashboardcomps/Dashboard components.js
+++ client/src/Components/objects/Userdashboardcomps/Dashboard components.js
@@ -759,7 +759,9 @@ const UserOrders = ({ State, Loader, setLoader }) => {
 
     if (State === 2) {
         return (
+            <div className="Order-container">
             <div className="container-fluid">
+                <h1>Pending Orders</h1>
                 <Table className='table-cat' style={{ margin: "40px 0px 0px 0px" }}>
                     <TableHead>
                         <TableRow style={{ border: "2px solid black", margin: "0px", textAlign: "center" }}>
@@ -796,13 +798,53 @@ const UserOrders = ({ State, Loader, setLoader }) => {
                         }
                     </TableBody>
                 </Table>
+                </div>
+                <div className="container-fluid">   
+                <h1>Completed Orders</h1>
+                <Table className='table-cat' style={{ margin: "40px 0px 0px 0px" }}>
+                    <TableHead>
+                        <TableRow style={{ border: "2px solid black", margin: "0px", textAlign: "center" }}>
+                            <TableCell style={{ textAlign: "center", fontWeight: '600' }}>SN</TableCell>
+                            {/* <TableCell>Service</TableCell> */}
+                            <TableCell style={{ textAlign: "center", fontWeight: '600' }}>Category</TableCell>
+                            <TableCell style={{ textAlign: "center", fontWeight: '600' }}>Price</TableCell>
+                            <TableCell style={{ textAlign: "center", fontWeight: '600' }}>Address</TableCell>
+                            <TableCell style={{ textAlign: "center", fontWeight: '600' }}>Number</TableCell>
+                            <TableCell style={{ textAlign: "center", fontWeight: '600' }}>paymentMethod</TableCell>
 
+
+
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            orderdetails.map((data, index) => (
+
+
+                                <TableRow key={index} style={{ backgroundColor: "white" }}>
+                                    <TableCell>{a++}</TableCell>
+
+                                    {/* <TableCell><p>{data.Service}</p></TableCell> */}
+                                    <TableCell><p>{data.Category}</p> </TableCell>
+                                    <TableCell><p>{data.price}</p></TableCell>
+                                    <TableCell><p>{data.address}</p></TableCell>
+                                    <TableCell><p>{data.number}</p></TableCell>
+                                    <TableCell><p>{data.paymentMethod}</p></TableCell>
+                                </TableRow>
+
+
+                            ))
+                        }
+                    </TableBody>
+                </Table>
+                
             </div>
 
 
-
+            </div>
         )
     }
+
     else return null
 
 }
