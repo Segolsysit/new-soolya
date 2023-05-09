f915c7d76e212ee52598ee3f2d28663485d84091
senthilkumarjp <senthilkumarjp55.com>
2023-05-09 17:30:24 +0530

    push

--- server/models/service_done_otpModel.js
+++ server/models/service_done_otpModel.js
@@ -26,5 +26,26 @@ const serviceDoneOtpModel = new mongoose.Schema({
   }
   
 });
+const listof_serviceModel = new mongoose.Schema({
+    workLists: [{
+        subCategory: String,
+        price: String
+      }],
+  total:{
+    type:String,
+    required:true
+  },
+  user_email:{
+    type:String,
+    required:true
+  }
 
-module.exports= mongoose.model('serviceDoneotpModel', serviceDoneOtpModel);
+  
+});
+const workDone_Model = mongoose.model("workDone_Model", listof_serviceModel) 
+const serviceDone_OtpModel = mongoose.model("serviceDoneotpModel", serviceDoneOtpModel) 
+
+module.exports = {
+  serviceDone_OtpModel,
+  workDone_Model
+}
