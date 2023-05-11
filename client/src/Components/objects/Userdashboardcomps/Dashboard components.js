bf49089da68c8a962e626c6a4f9ccef4e644f864
Sanjay M <sanjaymanikandan21@gmail.com>
2023-05-11 12:07:03 +0530

    array change in bill

--- client/src/Components/objects/Userdashboardcomps/Dashboard components.js
+++ client/src/Components/objects/Userdashboardcomps/Dashboard components.js
@@ -13,6 +13,12 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Multiselect from 'multiselect-react-dropdown';
+import {toast} from 'react-toastify';
+import {Razorpay} from 'react-razorpay'
+
+
+
+
 const style = {
     position: 'absolute',
     top: '50%',
@@ -995,9 +1001,9 @@ const UserOrders = ({ State, Loader, setLoader }) => {
     const [myorders, setMyorders] = useState([])
     const [pending_order, setpending_order] = useState([])
     const [completed_order, setCompleted_order] = useState([])
-    const [completed_bill, setCompleted_bill] = useState([])
+    const [completedbill, setCompletedbill] = useState([])
 
-    const Razorpay = useRazorpay();
+    
 
 
     const token = cookies.jwt2;
@@ -1008,18 +1014,24 @@ const UserOrders = ({ State, Loader, setLoader }) => {
 
     const [open4, setOpen4] = useState(true);
 
-
+    console.log(completedbill);
     const handleClose4 = () => {
         setOpen4(true)
     }
     const handleOpen4 = (id) => {
-        // axios.get(`http://localhost:3001/booking_api/Completed_order/${id}`)
-        //     .then((res) => {
-        //         setCompleted_bill(res.data)
-        //         setSubcategory(res.data.workLists)
-        //         console.log(res.data);
-        //     })
-        setOpen4(false)
+        axios.get(`http://localhost:3001/booking_api/Completed_billing/${id}`)
+            .then((res) => {
+                console.log(res.data);
+                setCompletedbill([res.data])
+            
+                setSubcategory(res.data.workLists)
+                
+            })
+            .then(()=>{
+                    setOpen4(false)
+                
+            })
+            
         console.log(open4);
     }
 
@@ -1361,7 +1373,7 @@ const[subCategory,setSubcategory]=useState([])
                             <TableBody style={{width:'100%'}}>
 
                             {
-                            completed_bill.map((data) => (
+                            completedbill.map((data) => (
                                 data.workLists.map((Sub,secondindex)=>(
                                     //console.log(Sub.subCategory)
 
@@ -1376,7 +1388,7 @@ const[subCategory,setSubcategory]=useState([])
                             <TableRow>
                                 <TableCell style={{ backgroundColor: "grey", display: 'flex', alignItems: 'center',border:'none' }}><p style={{ margin: '0px',fontWeight:'600',color:'white' }}>Total</p></TableCell>
                                 {
-                                    completed_bill.map((data,index)=>(
+                                    completedbill.map((data,index)=>(
                                         <TableCell key={index} style={{ backgroundColor: "white" }}><p style={{margin:'0px'}}>{data.total}</p></TableCell>
                                     )
                                     )
@@ -1391,8 +1403,8 @@ const[subCategory,setSubcategory]=useState([])
                     </div>
                     <div style={{ display: "flex", gap: "5px" }}>
                         {
-                            completed_order.map((data,index)=>(
-                                <button hidden={data.paymentMethod==="onlinePayment"?false:true} className="Bill-btn1">Pay</button>
+                            completedbill.map((data,index)=>(
+                                <button hidden={data.paymentMethod==="onlinePayment"?false:true} onClick={pay} className="Bill-btn1">Pay</button>
                             ))
 
                         }
