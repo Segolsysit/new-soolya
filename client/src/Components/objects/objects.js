1f996d37ea04fa5b74b6023c5de092678308eee7
Sanjay M <sanjaymanikandan21@gmail.com>
2023-06-30 12:26:38 +0530

    security patch

--- client/src/Components/objects/objects.js
+++ client/src/Components/objects/objects.js
@@ -878,9 +878,14 @@ const UserDashboard=()=>{
 
     const[Loader,setLoader]=useState(false)
 
-     const token = cookies.jwt2;
+    //  const token = cookies.jwt2;
 
-    // const token = localStorage.getItem("ty");
+
+
+    
+    //hello
+
+     const token = localStorage.getItem("ty")
     console.log(token);
     const decodedToken = jwt_decode(token);
     const userId = decodedToken.id;
